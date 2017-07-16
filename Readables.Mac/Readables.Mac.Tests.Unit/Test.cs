580fae37c9beb730da478a30be25e27ffd7a0e5f
Mac <huszar@gmail.com>
2017-07-16 09:24:51 +0200

    Unit test project

---
+++ Readables.Mac/Readables.Mac.Tests.Unit/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+namespace Readables.Mac.Tests.Unit
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
