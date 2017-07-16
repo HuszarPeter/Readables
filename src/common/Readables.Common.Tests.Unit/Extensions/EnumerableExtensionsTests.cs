00df15b5b2783562054d8e6558d8febd0a263f32
Mac <huszar@gmail.com>
2017-07-16 11:40:05 +0200

    Add common project and enumerable extension

---
+++ src/common/Readables.Common.Tests.Unit/Extensions/EnumerableExtensionsTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using Readables.Common.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace Readables.Common.Tests.Unit.Extensions
+{
+    [TestFixture]
+    public static class EnumerableExtensionsTests
+    {
+        [Test]
+        public static void ShouldThrowException_When_EnumerableIsNull()
+        {
+            IEnumerable<string> enumerable = null;
+            Action act = () => enumerable.ForEach((item) => { });
+            act.ShouldThrow<ArgumentNullException>().Which.ParamName.Should().Be("enumerable");
+        }
+
+        [Test]
+        public static void ShouldThrowExcetion_When_ActionIsNull()
+        {
+            IEnumerable<string> enumerable = new List<string>
+            {
+                "item1",
+                "item2"
+            };
+            Action act = () => enumerable.ForEach(null);
+            act.ShouldThrow<ArgumentNullException>().Which.ParamName.Should().Be("action");
+        }
+    }
+}
---
+++ src/common/Readables.Common/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Readables.Common.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+        {
+            if (enumerable == null)
+            {
+                throw new ArgumentNullException(nameof(enumerable));
+            }
+
+            if (action == null)
+            {
+                throw new ArgumentNullException(nameof(action));
+            }
+
+            foreach(var item in enumerable)
+            {
+                action(item);
+            }
+        }
+    }
+}
