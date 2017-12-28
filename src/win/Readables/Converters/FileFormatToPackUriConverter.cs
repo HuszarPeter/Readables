6537c05e0f1e777f43054e2dad025d64387ac47d
Mac <huszar@gmail.com>
2017-12-28 10:35:53 +0100

    File format images in listview

---
+++ src/win/Readables/Converters/FileFormatToPackUriConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Readables.Converters
+{
+    public class FileFormatToPackUriConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is String str)
+            {
+                switch (str)
+                {
+                    case "epub":
+                        return "pack://application:,,,/Resources/format_epub.png";
+                    case "Digital comic":
+                        return "pack://application:,,,/Resources/format_comic.png";
+                    case "Mobi":
+                        return "pack://application:,,,/Resources/format_azw3.png";
+                }
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
